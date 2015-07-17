c98e2452efb674dc58c3a543a725dc8ce7055f3e
Adam Hooper <adam@adamhooper.com>
2015-07-17 10:48:27 -0400

    rm in deploy-hook. Might fix our problem?

--- deploy/post-deploy-hook.sh
+++ deploy/post-deploy-hook.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-DIR="$(pwd)"/"$(dirname "$0")"/..
-
 set -x
 set -e
+
+rm -f data/users.csv data/votes.csv
+touch /mnt/policy-vote/users.csv /mnt/policy-vote/votes.csv
+ln -s /mnt/policy-vote/users.csv data/users.csv
+ln -s /mnt/policy-vote/votes.csv data/votes.csv
 
 npm install --production
 
 node_modules/.bin/gulp
-touch /mnt/policy-vote/users.csv /mnt/policy-vote/votes.csv
-ln -s /mnt/policy-vote/users.csv "$DIR"/data/users.csv
-ln -s /mnt/policy-vote/votes.csv "$DIR"/data/votes.csv
 
 export APPLICATION_SECRET=`cat /opt/policy-vote/APPLICATION_SECRET`
 pm2 startOrReload ecosystem.json --env production -i 1"
